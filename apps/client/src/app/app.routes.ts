fbf2550c0c4c4f6ce0ff822bb1f1d64e941a3858
Bryan Arévalo <bryantello2010@hotmail.com>
2023-11-26 14:33:10 -0600

    Added unauthorizedGuard

--- apps/client/src/app/app.routes.ts
+++ apps/client/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { LoginComponent } from './pages/auth/login/login.component';
 import { AuthSkeleton } from './core/skeletons/auth/auth.skeleton.component';
 import { RegisterComponent } from './pages/auth/register/register.component';
 import { authGuard } from './guards/auth.guard';
+import { unauthorizedGuard } from './guards/unauthorized.guard';
 
 export const routes: Routes = [
   {
     path: 'auth',
     component: AuthSkeleton,
+    canActivate: [unauthorizedGuard],
     children: [
       { path: 'login', component: LoginComponent },
       {
