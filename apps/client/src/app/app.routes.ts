c594ec268c9ac1e00267de824c94ac16d3065411
Bryan Arévalo <bryantello2010@hotmail.com>
2023-11-26 13:20:51 -0600

    Added register componetent loginComponent -> auth/loginComponent

--- apps/client/src/app/app.routes.ts
+++ apps/client/src/app/app.routes.ts
@@ -1,13 +1,20 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
+import { LoginComponent } from './pages/auth/login/login.component';
 import { AuthSkeleton } from './core/skeletons/auth/auth.skeleton.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
 
 export const routes: Routes = [
   { component: HomeComponent, path: '' },
   {
     path: 'auth',
     component: AuthSkeleton,
-    children: [{ path: 'login', component: LoginComponent }],
+    children: [
+      { path: 'login', component: LoginComponent },
+      {
+        path: 'register',
+        component: RegisterComponent,
+      },
+    ],
   },
 ];
