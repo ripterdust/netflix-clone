00fd8ce657987c47f00224de2312e8fcf6cd75f8
Bryan Arévalo <bryantello2010@hotmail.com>
2023-11-26 14:54:45 -0600

    Added primeng styles

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, RouterOutlet } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    InputTextModule,
+    ButtonModule,
+    CardModule,
+  ],
   template: '<router-outlet/>',
-  styleUrls: [],
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'client';
